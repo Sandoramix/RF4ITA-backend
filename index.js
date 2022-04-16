0a2f7149b3a6157268962caad4cd02401e7e7eb3
sandoramix <sandoramixer61@gmail.com>
2022-04-16 21:45:42 +0200

    express app with cors and router

---
+++ index.js
@@ -0,0 +1,44 @@
+const env = require(`./config.json`);
+
+const path = require(`path`);
+const fs = require(`fs`);
+
+const express = require(`express`);
+const http = require(`http`);
+const cors = require(`cors`);
+
+const app = express(
+    cors({
+        origin: `*`,
+    })
+);
+
+const http_server = http.createServer(app).listen(env.server.port, env.server.host, () => {
+    console.log(`listening on http://localhost:${env.server.port}`);
+});
+
+// app.use(`/`,(req,res)=>{
+// 	res.redirect(`/maps`)
+// })
+app.use(`/leaflet`, express.static(`./node_modules/leaflet/dist/`));
+app.use(`/`, express.static(path.join(__dirname, `public`)));
+
+var endpoints = {};
+getHandlers();
+
+function getHandlers() {
+    var module_files = fs.readdirSync(path.resolve(__dirname, env.routes_dir), {
+        withFileTypes: true,
+    });
+    module_files.forEach((file) => {
+        if (!file.name.endsWith(`.js`)) {
+            return;
+        }
+        let handler_file = require(path.join(__dirname, env.routes_dir, file.name).replace(`.js`, ``));
+        let route = handler_file.base_route;
+        endpoints[route] = handler_file.endpoints;
+        let handler = handler_file.handler();
+
+        app.use(route, handler);
+    });
+}
