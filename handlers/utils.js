4d3783dcaa380611c4f64d157521ac9c9801eb1d
sandoramix <sandoramixer61@gmail.com>
2022-05-07 20:20:45 +0200

    .

--- handlers/utils.js
+++ handlers/utils.js
@@ -1,9 +1,9 @@
 function errorCatch(response, code, message) {
-    return response.status(code).json({
-        message: message,
-    });
+	return response.status(code).json({
+		message: message,
+	});
 }
 
 module.exports = {
-    errorCatch,
+	errorCatch,
 };
